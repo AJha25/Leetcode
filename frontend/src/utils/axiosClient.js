347fd90ef5c5bbc1050b2a3d546cb96a55471f62
AJha25 <abjbsj242002@gmail.com>
2025-09-08 21:27:30 +0530

    Prepare for fullstack Vercel deployment

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,13 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+const axiosClient = axios.create({
+  // Use env var if set, otherwise fallback to `/api`
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
-
 export default axiosClient;
-
